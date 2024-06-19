e8bdbb4a7c4e2db8e004dcbff44009affa45f0dc
Sanket Dadali <sanketdadali05@gmail.com>
2024-06-19 19:45:14 +0530

    Unnnecessary files removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
-import { createClient } from "@/utils/supabase/server";
+
 import LandingPage from "@/components/LandingPage";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
 
   return (
     <div>
