c3febcb48f8474ed93fb45a40d126ec3c344bccd
Sanket Dadali <sanketdadali05@gmail.com>
2024-08-27 19:45:46 +0530

    env updated

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_SUPABASE_SECRET!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SECRET!;
 export const supabase = createClient(supabaseUrl, supabaseKey);
